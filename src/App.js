0fa9bed0eb497afe1c37fc133336a6cc67af3f23
Logan Negley <logannegley@gmail.com>
2020-03-02 19:23:47 -0600

    fixing bug in  app.js with going back from begining of connect

--- src/App.js
+++ src/App.js
@@ -8,22 +8,22 @@ import {connect } from 'react-redux';
 
 const App = (props) => {
   console.log(props)
-  // const state = {
-  //   additionalPrice: 0,
-  //   car: {
-  //     price: 26395,
-  //     name: '2019 Ford Mustang',
-  //     image:
-  //       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-  //     features: []
-  //   },
-  //   additionalFeatures: [
-  //     { id: 1, name: 'V-6 engine', price: 1500 },
-  //     { id: 2, name: 'Racing detail package', price: 1500 },
-  //     { id: 3, name: 'Premium sound system', price: 500 },
-  //     { id: 4, name: 'Rear spoiler', price: 250 }
-  //   ]
-  // };
+  const state = {
+    additionalPrice: 0,
+    car: {
+      price: 26395,
+      name: '2019 Ford Mustang',
+      image:
+        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      features: []
+    },
+    additionalFeatures: [
+      { id: 1, name: 'V-6 engine', price: 1500 },
+      { id: 2, name: 'Racing detail package', price: 1500 },
+      { id: 3, name: 'Premium sound system', price: 500 },
+      { id: 4, name: 'Rear spoiler', price: 250 }
+    ]
+  };
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
@@ -48,13 +48,13 @@ const App = (props) => {
 };
 
 // mapStateToProps goes here!!!!
-const mapStateToProps = state => {
-  return {
-    additionalPrice: state.additionalPrice,
-    car: state.car,
-    additionalFeatures: state.additionalFeatures,
-  };
-};
+// const mapStateToProps = state => {
+//   return {
+//     additionalPrice: state.additionalPrice,
+//     car: state.car,
+//     additionalFeatures: state.additionalFeatures,
+//   };
+// };
 
-export default connect(mapStateToProps, {})(App);
+export default connect(null, {})(App);
  
